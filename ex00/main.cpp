44b80ccd6fc8cad17000c1a9988cddd8428e620c
ggjulio <juligonz@student.42.fr>
2021-02-06 06:42:24 +0100

    ex00 seem done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juligonz <juligonz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 04:50:57 by juligonz          #+#    #+#             */
-/*   Updated: 2021/02/06 05:54:29 by juligonz         ###   ########.fr       */
+/*   Updated: 2021/02/06 06:41:31 by juligonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <iostream>
 
 int main(){
+	{
 	Sorcerer robert("Robert", "the Magnificent");
 	Victim jim("Jimmy");
 	Peon joe("Joe");
@@ -23,6 +24,36 @@ int main(){
 	std::cout << robert << jim << joe;
 	robert.polymorph(jim);
 	robert.polymorph(joe);
+	}
+	std::cout << "##################### BORING ####################" << std::endl;
+	{
+	Sorcerer  *robert = new Sorcerer("Robert", "the Magnificent");
+	Victim *jim = new Victim("Jimmy");
+	Peon *joe = new Peon("Joe");
 	
+	std::cout << *robert << *jim << *joe;
+	robert->polymorph(*jim);
+	robert->polymorph(*joe);
+	delete joe;
+	delete jim;
+	delete robert;
+	}
+	std::cout << "##################### Still BORING ####################" << std::endl;
+	{
+	Victim *jim = new Peon("Joe");
+		
+	delete jim;
+	}
+	std::cout << "################## Still, still BORING ####################" << std::endl;
+	{
+	Peon jim("Joe");
+	Victim &victim = jim;
+
+	victim.polymorphed();	
+	}
+	std::cout << "################## Still, still still...  #################" << std::endl;
+	{
+
+	}
 	return 0;
 }
